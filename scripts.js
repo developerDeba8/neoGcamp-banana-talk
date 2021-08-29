a6b76776739e73705365bc1d99370c7ecab9ddad
Debashis Saha <developerdeba8@gmail.com>
2021-08-29 14:59:41 +0530

    ex09-10: fetch and error handling

--- scripts.js
+++ scripts.js
@@ -2,10 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
 var outputDivText = document.querySelector("#txt-output")
 
-function translateButtonClickHandler(){
-    console.log("clicked");
-    console.log("Input entered in text-area: ", inputText.value);
-    outputDivText.innerText = inputText.value;
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function constructTranslationTextURL(text){
+    return serverURL + "?" + "text=" + text;
 }
+
+function errorHandler(error){
+    console.log("Error occured: ", error);
+}
+
+function translateButtonClickHandler(){
+    fetch(constructTranslationTextURL(inputText.value))
+    .then(response => response.json())
+    .then(json => {
+        outputDivText.innerText = json.contents.translated;})
+    .catch(errorHandler)
+}
+
 
 btnTranslate.addEventListener("click", translateButtonClickHandler);
