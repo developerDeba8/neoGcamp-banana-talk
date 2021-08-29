730c50d78859579b96dc2cbee594e1540d2c11d8
Debashis Saha <developerdeba8@gmail.com>
2021-08-29 12:51:11 +0530

    ex04: wiring textarea input

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#txt-input");
 
 function translateButtonClickHandler(){
     console.log("clicked");
+    console.log("Input entered in text-area: ", inputText.value);
 }
 
 btnTranslate.addEventListener("click", translateButtonClickHandler);
