15ffc7ce692e62f99693aa877cd9bf93289bbb84
Debashis Saha <developerdeba8@gmail.com>
2021-08-29 13:26:45 +0530

    ex05: wire a div to show output

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var inputText = document.querySelector("#txt-input");
+var outputDivText = document.querySelector("#txt-output")
 
 function translateButtonClickHandler(){
     console.log("clicked");
     console.log("Input entered in text-area: ", inputText.value);
+    outputDivText.innerText = inputText.value;
 }
 
 btnTranslate.addEventListener("click", translateButtonClickHandler);
